b78dd02cd878f1da1c1dc775ab02781517ea8456
philippe ravette <virauteuil1@gmail.com>
2021-08-05 19:56:35 +0200

    modif index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 //fetch de l'URL//
-fetch(url)
-    .then(function(res) {
-        return res.json();
-    })
-    .catch((error) => {
-        let productsContainer = document.querySelector(".products-container");
-        productsContainer.innerHTML =
-            "Nous n'avons pas réussi à afficher nos nounours. Avez vous bien lancé le serveur local (Port 3000) ? <br>Si le problème persiste, contactez-nous.";
-        productsContainer.style.textAlign = "center";
-        productsContainer.style.padding = "30vh 0";
-    })
