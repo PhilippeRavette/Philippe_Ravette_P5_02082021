7eac4c5f840620b40e367db2846ff5d1fd2161df
philippe ravette <virauteuil1@gmail.com>
2021-08-12 19:02:25 +0200

    debut page produit

--- index.js
+++ index.js
@@ -44,19 +44,19 @@ function addCards(data) {
         const price = convertPrice(Product.price);
         card.innerHTML += `
       <div class="col-sm-12 col-md-6 col-lg-6 pb-3"> 
-          <div class="card border bg-warning shadow p-3 mb-5 rounded">
+          <div class="card border bg-secondary shadow p-3 mb-5 rounded">
               <div class="card-body"> 
                   <div class="row">
                       <a href="./produit.html?_id=${produit._id}"><img src="${produit.imageUrl}" class="img-fluid img-thumbnail bg-black p-1" style="--bs-bg-opacity: .50;" alt="${produit.name}"></a>
                       <div class="col-6 col-sm-7 mt-3" >
-                          <h3 class="card-title">${produit.name}</h3>
+                          <h3 class="card-title text-white">${produit.name}</h3>
                       </div>
                       <div class="col-6 col-sm-5 text-end mt-3">
-                          <h3 class="card-title">${price}</h3>
+                          <h3 class="card-title text-white">${price}</h3>
                       </div>
                   </div>
                   
-                  <a href="./produit.html?_id=${produit._id}" class="btn btn-secondary my-2 d-flex justify-content-center fs-4">Acheter ce produit</a>
+                  <a href="./produit.html?_id=${produit._id}" class="btn btn-success my-2 d-flex justify-content-center fs-4">Acheter ce produit</a>
               </div>
           </div>
       </div>`;
