68e46abb52ae50db67168d6fda9839d1ed2deee8
philippe ravette <virauteuil1@gmail.com>
2021-08-12 18:07:30 +0200

    apparition dynamique produit

--- function.js
+++
@@ -1,5 +0,0 @@
-//Variables Globales//
-const url = "https://localhost:3000/api/teddies";
-
-//Fonction conversion de prix//
-let price
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const url = "http://localhost:3000/api/teddies";
+//A $(document).ready()block.
+$(document).ready(function() { console.log("ready!") });
 
 //fetch de l'URL
 fetch(url)
@@ -23,7 +25,7 @@ class Product {
 
 // convertir le prix
 function convertPrice(productPrice) {
-    let price = `${productPrice}`;
+    let price = `${produit.price}`;
     price = Intl.NumberFormat("fr-FR", {
         style: "currency",
         currency: "EUR",
@@ -34,27 +36,27 @@ function convertPrice(productPrice) {
 
 // fonction pour la création des cards de la page d'accueil
 function addCards(data) {
-    //boucle pour chaque iteration d'un produit
+    //boucle pour chacun d'un produit
     for (produit of data) {
         //recupère l'élément liste dans le HTML
         const card = document.getElementById("liste");
         //convertit le prix
         const price = convertPrice(Product.price);
         card.innerHTML += `
-      <div class="col-sm-12 col-md-6 col-lg-6 pb-3  ">
-          <div class="card border bg-light shadow p-3 mb-5 bg-body rounded">
-              <div class="card-body">
+      <div class="col-sm-12 col-md-6 col-lg-6 pb-3"> 
+          <div class="card border bg-warning shadow p-3 mb-5 rounded">
+              <div class="card-body"> 
                   <div class="row">
-                      <a href="./produit.html?_id=${produit._id}"><img src="${produit.imageUrl}" class="img-fluid img-thumbnail p-1" alt="${produit.name}"></a>
+                      <a href="./produit.html?_id=${produit._id}"><img src="${produit.imageUrl}" class="img-fluid img-thumbnail bg-black p-1" style="--bs-bg-opacity: .50;" alt="${produit.name}"></a>
                       <div class="col-6 col-sm-7 mt-3" >
-                          <h5 class="card-title">${produit.name}</h5>
+                          <h3 class="card-title">${produit.name}</h3>
                       </div>
                       <div class="col-6 col-sm-5 text-end mt-3">
-                          <h5 class="card-title">${price}</h5>
+                          <h3 class="card-title">${price}</h3>
                       </div>
                   </div>
-                  <p class="card-text text-truncate">${produit.description}</p>
-                  <a href="./produit.html?_id=${produit._id}" class="btn btn-secondary">Acheter ce produit</a>
+                  
+                  <a href="./produit.html?_id=${produit._id}" class="btn btn-secondary my-2 d-flex justify-content-center fs-4">Acheter ce produit</a>
               </div>
           </div>
       </div>`;
